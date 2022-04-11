b2c836e3d3bcc86cc89165f63b41334bb6ad9b18
NaveenKumarMFRL <naveenkumar.m@focusresearchlabs>
2022-04-11 15:20:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log('Your Value') it print what u to show
+// node 'file name'
+console.log(20 + 30);
+
+// varibles
+
+// const , var , let
+
+// stingName = "ram";
+//print("stingName");
+
+const stingName = "Ram";
+
+// array
+
+const Arrays = ["naveen", "sam"];
+
+// object
+
+const NewObjects = {
+  name: "Naveen",
+  age: 23,
+};
+
+//json
+
+const ArrayObject = [
+  {
+    name: "Naveen",
+    age: 23,
+  },
+  {
+    name: "same",
+    age: 25,
+  },
+];
+
+// i++ = i+1
+// for loop
+for (var i = 0; i <= 10; i++) {
+  console.log("i", Arrays[i]);
+}
+// if else condition
+
+const num = 20;
+
+if (num <= 10) {
+  console.log("num is greater than 10");
+} else {
+  console.log("num is lesser than 10");
+}
+
+// Function
+function nav() {
+  console.log("first Console");
+}
+
+nav();
+
+console.log(ArrayObject);
